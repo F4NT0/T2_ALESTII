18736a90ce892fec27922d2cf12b425473732ed7
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-05-28 00:33:43 -0300

    castelo

---
+++ Program/src/castelo.py
@@ -0,0 +1,13 @@
+class Castelo():
+    def __init__(self,nro_soldados,nro_castelo):
+        """Classe para definir como o Castelo Funciona"""
+        self.nro_castelo = nro_castelo
+        self.nro_soldados = nro_soldados
+    
+    # Método onde desejo definir os Castelos conectados
+    def rotas(self,Castelo castelo, lista_castelos):
+        lista_castelos.append(castelo)
+    
+    
+    
+
